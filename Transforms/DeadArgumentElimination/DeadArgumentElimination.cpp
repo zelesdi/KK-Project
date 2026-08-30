3a4226d25ab8bdcc63ba0cd3debcb0f6fdee1400
zelesdi <mtasic055@gmail.com>
2026-08-30 03:03:15 +0200

    Implement function cloning

--- Transforms/DeadArgumentElimination/DeadArgumentElimination.cpp
+++ Transforms/DeadArgumentElimination/DeadArgumentElimination.cpp
@@ -1,7 +1,6 @@
-#include <unordered_set>
-
 #include "llvm/ADT/Statistic.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
 using namespace llvm;
@@ -9,59 +8,98 @@ using namespace llvm;
 
 namespace {
   struct DeadArgumentElimination : public FunctionPass {
+    std::vector<bool> ArgIsDead;
     std::vector<Type *> ArgTypes;
     std::unordered_map<BasicBlock *, BasicBlock *> BasicBlocksMap;
+    std::unordered_map<Value *, Value *> VariablesMap;
 
     static char ID;
     DeadArgumentElimination() : FunctionPass(ID) {}
 
     void findDeadArguments(Function &F)
     {
+      ArgIsDead.assign(F.arg_size(), false);
+      size_t i = 0;
       for (Argument &Arg : F.args()) {
         // .use_empty() returns true if Arg is dead
-        bool ArgIsDead = Arg.use_empty();
-        if (!ArgIsDead) {
+        if (!(ArgIsDead[i] = Arg.use_empty())) {
           ArgTypes.push_back(Arg.getType());
         }
+        i++;
       }
     }
 
-    Function *createNewFunction(Function &F)
+    Function *createNewFunction(Function &OldFunc)
     {
-      Type *Ty = F.getType();
+      Type *Ty = OldFunc.getFunctionType()->getReturnType();
 
       FunctionType *NewFuncTy = FunctionType::get(Ty,ArgTypes,false);
 
       Function *newFunc = Function::Create(
         NewFuncTy,
         Function::ExternalLinkage,   // function can be seen outside the current module
-        "new_" + F.getName(),
-        F.getParent()
+        "new_" + OldFunc.getName(),
+        OldFunc.getParent()
       );
 
       return newFunc;
     }
 
-    void mapBasicBlocks(Function &F, Function *NewFunc)
+    void mapBasicBlocks(Function &OldFunc, Function *NewFunc)
     {
-      for (BasicBlock &BB : F) {
+      for (BasicBlock &BB : OldFunc) {
         BasicBlocksMap[&BB] =
-          BasicBlock::Create(F.getContext(),"", NewFunc);
+          BasicBlock::Create(OldFunc.getContext(),"", NewFunc);
       }
     }
 
-    void cloneFunctionBody(Function &F, Function *NewFunc)
+    void mapVariables(Function &OldFunc, Function *NewFunc)
     {
-      mapBasicBlocks(F, NewFunc);
+      Argument *NewArg = NewFunc->arg_begin();
+      size_t i = 0;
+      for (Argument &OldArg : OldFunc->args()) {
+        if (!ArgIsDead[i]) {
+          VariablesMap[&OldArg] = NewArg;
+          NewArg++;
+        }
+        i++;
+      }
+    }
 
-      BasicBlock *NewBB;
-      for (BasicBlock &BB : F) {
-        NewBB = BasicBlocksMap[&BB];
+    void cloneFunctionBody(Function &OldFunc, Function *NewFunc)
+    {
+      mapBasicBlocks(OldFunc, NewFunc);
+      mapVariables(OldFunc, NewFunc);
 
-        Instruction *NewInstr;
+      // clone instructions from old basic blocks into new ones
+      // map instructions
+      for (BasicBlock &BB : OldFunc) {
+        BasicBlock *NewBB = BasicBlocksMap[&BB];
         for (Instruction &Instr : BB) {
-          NewInstr = Instr.clone();
+          Instruction *NewInstr = Instr.clone();
           NewInstr->insertInto(NewBB, NewBB->end());
+          VariablesMap[&Instr] = NewInstr;
+        }
+      }
+
+      for (BasicBlock &BB : OldFunc) {
+        for (Instruction &Instr : BB) {
+          Instruction *NewInstr = cast<Instruction>(VariablesMap[&Instr]);
+          for (unsigned i = 0; i < NewInstr->getNumOperands(); i++) {
+            Value *Operand = NewInstr->getOperand(i);
+
+            auto It = VariablesMap.find(Operand);
+            if (It != VariablesMap.end()) {
+              NewInstr->setOperand(i, It->second);
+            }
+            else if (auto *OpBB = dyn_cast<BasicBlock>(Operand)) {
+              auto It2 = BasicBlocksMap.find(OpBB);
+              if (It2 != BasicBlocksMap.end()) {
+                NewInstr->setOperand(i, It2->second);
+              }
+            }
+            // TODO: PHI Nodes
+          }
         }
       }
     }
@@ -69,6 +107,7 @@ namespace {
     bool runOnFunction(Function &F) override {
       ArgTypes.clear();
       BasicBlocksMap.clear();
+      VariablesMap.clear();
 
       findDeadArguments(F);
 
