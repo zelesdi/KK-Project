31806358e2af943d03d3fbb204e2a55f4d213738
Mihajlo Tasic <116030514+zelesdi@users.noreply.github.com>
2026-09-02 09:42:19 +0200

    Update DSE DAE example
    Refactor functions to eliminate dead stores and unused arguments.

--- DSE_DAE_example.c
+++ DSE_DAE_example.c
@@ -1,16 +1,78 @@
-#include <stdio.h>
+int unused_argument(int a, int b)
+{
+    return a;
+}
 
-int foo(int a, int b)
+int partially_unused(int a, int b, int c)
+{
+    int x = a + 10;
+    return x;
+}
+
+int dead_stores(int a)
 {
     int x;
 
-    x = a;
     x = 10;
+    x = 20;
 
     return x;
 }
 
-int main()
+int multiple_dead_stores(int a)
 {
-    return foo(5, 20);
+    int x = 5;
+    int y = 10;
+
+    x = a + 1;
+    y = x + 2;
+
+    x = 100;
+
+    return 42;
+}
+
+int useful_and_dead_stores(int a)
+{
+    int x;
+
+    x = 10;
+    x = a + 5;
+    x = 20;
+
+    return x;
+}
+
+int combined(int a, int b, int c)
+{
+    int x;
+
+    x = a;
+    x = 100;
+
+    return b;
+}
+
+int only_first(int a, int b, int c)
+{
+    int x = a;
+
+    return x;
+}
+
+int no_dead_store(int a)
+{
+    int x = a + 10;
+
+    return x;
+}
+
+int g;
+ 
+void f() {
+    g = 5;
+}
+
+void f(int *p) {
+    *p = 5;
 }
