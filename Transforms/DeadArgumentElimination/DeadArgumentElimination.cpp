214321d5d35dd2829696964da6e1d0aab92ecb69
Mihajlo Tasic <116030514+zelesdi@users.noreply.github.com>
2026-09-02 09:46:29 +0200

    Reorder include statements in DeadArgumentElimination.cpp

--- Transforms/DeadArgumentElimination/DeadArgumentElimination.cpp
+++ Transforms/DeadArgumentElimination/DeadArgumentElimination.cpp
@@ -1,14 +1,13 @@
 #include <vector>
 #include <unordered_map>
 
-#include <llvm/IR/Constants.h>
-
 #include "llvm/ADT/Statistic.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Constants.h"
 
 using namespace llvm;
 
